3c5efe1cf1d1a9bc7e660f887dd22067349d8761
ankurkumar222 <ankur.cse.bvcoe@gmail.com>
2015-12-18 19:20:05 -0800

    commit

---
+++ String/src/Code/String/SplitDemo.java
@@ -0,0 +1,14 @@
+package Code.String;
+
+import java.util.regex.*;
+import java.util.*;
+import static util.Print.*;
+
+public class SplitDemo {
+	public static void main(String[] args) {
+		String input = "This!!unusual use!!of exclamation!!points";
+		print(Arrays.toString(Pattern.compile("!!").split(input)));
+		// Only do the first three:
+		print(Arrays.toString(Pattern.compile("!!").split(input, 2)));
+	}
+} 
