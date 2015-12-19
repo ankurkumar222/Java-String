2fa939fe8d8a9cd88ee97a0f7090a3fa525427ad
ankurkumar222 <ankur.cse.bvcoe@gmail.com>
2015-12-18 17:05:47 -0800

    commit

---
+++ String/src/Code/String/Immutable.java
@@ -0,0 +1,18 @@
+package Code.String;
+
+import static util.Print.print;
+
+public class Immutable {
+
+	public static String upcase(String s) {
+		return s.toUpperCase();
+	}
+
+	public static void main(String[] args) {
+		String q = "howdy";
+		print(q);
+		String qq = upcase(q);
+		print(qq);
+		print(q);
+	}
+}
